e5a9e645b4a92d821d872a7bd352b3057ab8b1fb
Damianleonh <rleon2@ucol.mx>
2022-04-27 14:47:19 -0500

    a

--- src/components/Registro.js
+++ src/components/Registro.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { Text, SafeAreaView, View, Image, TextInput, StyleSheet, Pressable, Modal } from 'react-native'
 
-const Registro = ({ modalRegistro,setModalRegistro} ) => {
+const Registro = ({ modalRegistro, setModalRegistro }) => {
   return (
     <SafeAreaView>
+
+
       {/* Boton cerrar modal*/}
       <Pressable
-      style={styles}
+        style={styles.btnRegresar}
+        onPress={() => setModalRegistro(!modalRegistro)}
       >
-        <Text>Volver</Text>
+        <Text
+          style={styles.textoBtnRegresar}
+        >
+           Cancelar</Text>
       </Pressable>
 
       <Image
@@ -27,7 +33,12 @@ const Registro = ({ modalRegistro,setModalRegistro} ) => {
         <TextInput
           style={styles.input}
           placeholder='Contrasena'
-          
+          secureTextEntry='true'
+        />
+        <TextInput
+          style={styles.input}
+          placeholder='Repetir Csontrasena'
+          secureTextEntry='true'
         />
 
 
@@ -43,7 +54,7 @@ const Registro = ({ modalRegistro,setModalRegistro} ) => {
             </Text>
           </Pressable>
 
-          
+
         </View>
 
       </View>
@@ -57,57 +68,73 @@ const Registro = ({ modalRegistro,setModalRegistro} ) => {
   )
 }
 const styles = StyleSheet.create({
-    imagen:{
-        width: 200,
-        height: 100
-    },
+  imagen: {
+    width: 200,
+    height: 100
+  },
 
-    input:{ 
-        paddingHorizontal:15,
-        paddingVertical: 10,
-        borderRadius:10,
-        borderColor: '#E3F4FF',
-        borderWidth:3,
-        marginTop: 15
-    },
+  input: {
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    borderColor: '#E3F4FF',
+    borderWidth: 3,
+    marginTop: 15
+  },
 
-    contenedorBtnInputs:{
-        marginHorizontal: 30
-    },
+  contenedorBtnInputs: {
+    marginHorizontal: 30
+  },
 
-    button:{
-        backgroundColor: '#0F74F2',
-        width: 200,
-        padding: 15,
-        borderRadius:10,
-        alignItems:'center',
-        marginTop: 40
-    },
+  button: {
+    backgroundColor: '#0F74F2',
+    width: 200,
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 40
+  },
 
-    button2:{
-        backgroundColor: '#D9DEFB',
-        width: 200,
-        padding: 15,
-        borderRadius:10,
-        alignItems:'center',
-        marginTop: 20
-    },
+  button2: {
+    backgroundColor: '#D9DEFB',
+    width: 200,
+    padding: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    marginTop: 20
+  },
 
-    buttonText:{
-        color:'#fff',
-        fontWeight: 'bold',
-        fontSize: 20      
-    },
+  buttonText: {
+    color: '#fff',
+    fontWeight: 'bold',
+    fontSize: 20
+  },
 
-    buttonText2:{
-        color:'#002060',
-        fontWeight: 'bold',
-        fontSize: 20      
-    },
+  buttonText2: {
+    color: '#002060',
+    fontWeight: 'bold',
+    fontSize: 20
+  },
 
-    contenedorBotones:{
-        alignItems: 'center'
-    }
+  contenedorBotones: {
+    alignItems: 'center'
+  },
+
+  btnRegresar: {
+    width: 100,
+    alignItems: 'center',
+    marginHorizontal: 15,
+    marginTop:10
+  },
+
+  textoBtnRegresar: {
+    color: '#0F74F2',
+    fontWeight: '400',
+    fontSize: 17
+  }
+
+
+
 })
 
 export default Registro
