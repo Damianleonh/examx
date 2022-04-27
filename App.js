ff44b3abf603c880960400775e2f3fa6cf8a8d32
javierorozco2 <eorozco8@ucol.mx>
2022-04-27 10:28:06 -0500

    nuevo

--- App.js
+++ App.js
@@ -1,28 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Login from './screens/Login';
-import Home from './screens/Home';
+import React from 'react'
+import { Text, SafeAreaView, View, Image, TextInput, StyleSheet } from 'react-native'
 
-const Stack = createNativeStackNavigator();
+const App = () => {
+    return (
+        <SafeAreaView>
+            <Image
+                style={styles.imagen}
+                source={ require('./sources/Images/logo.jpg') }
+            />
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen options={{headerShown:false}} name="Login" component={Login} />
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+            <TextInput
+                placeholder='Correo'
+                keyboardType='email-address'
+            />
+
+            <TextInput
+                placeholder='Correo'
+                keyboardType='email-address'
+            />
+
+        </SafeAreaView>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    imagen:{
+        width: 200,
+        height: 100
+    }
+})
+
+
+
+export default App
