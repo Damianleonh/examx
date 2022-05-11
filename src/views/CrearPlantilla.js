83d7308ae4e17b2de4a5d3c4fdd36d8466603ce5
javierorozco2 <eorozco8@ucol.mx>
2022-05-11 08:55:59 -0500

    solucion de errores

--- src/views/CrearPlantilla.js
+++ src/views/CrearPlantilla.js
@@ -1,116 +1,167 @@
-import React,{useState} from 'react'
-import {
-    SafeAreaView,
-    ScrollView,
-    StatusBar,
-    StyleSheet,
-    Text,
-    useColorScheme,
-    View,
-    Image,
-    RadioButton
-} from 'react-native';
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, ScrollView, StyleSheet, View, Image, Pressable, TextInput} from "react-native";
+import { auth } from '../../database/firebase'
+import { RadioButton, Text } from 'react-native-paper';
+import DropDownPicker from "react-native-dropdown-picker";
 
-const radioButtonsData = [{
-    id: '1', 
-    label: 'Option 1',
-    value: 'option1'
-}, {
-    id: '2',
-    label: 'Option 2',
-    value: 'option2'
-}]
+const CrearPlantilla = () => {
+ 
+    useEffect(() => {
+      var date = new Date().getDate(); //Current Date
+      var month = new Date().getMonth() + 1; //Current Month
+      var year = new Date().getFullYear(); //Current Year
+      var hours = new Date().getHours(); //Current Hours
+      var min = new Date().getMinutes(); //Current Minutes
 
-import { RadioGroup } from 'react-native-radio-buttons-group';
-import Pressable from 'react-native/Libraries/Components/Pressable/Pressable';
-function CrearPlantilla() {
-    const [radioButtons, setRadioButtons] = useState(radioButtonsData)
+      if(date<10){
+          date = "0" + date
+      }
+      if(month<10){
+          month = "0" + month
+      }
+      if(hours < 10){
+          hours = "0" + hours
+      }
+      if(min < 10){
+          min = "0" + min
+      }
 
-    function onPressRadioButton(radioButtonsArray) {
-        setRadioButtons(radioButtonsArray);
-    }
+      setDate({
+          dateDay: date + '/' + month + '/' + year,
+          dateHour: hours + ':' + min
+      }
+      );
+    }, []);
 
-  return (
-    <SafeAreaView>
-      <ScrollView>
-        <View style={styles.containerF}>
-         <Text style={styles.txtForm}>Formulario 1</Text>
-         <Text style={styles.txtFecha}>Fecha</Text>
-         <View style={styles.containerP}>
-             <View style={styles.containerOps}>
-                 <Text style={styles.txtOps}>¿Pregunta 1?</Text>
-                 <Text style={styles.txtOps2}>5 Seg </Text>
-                 <Image style={{width:25,height:25,marginRight:10}} source={require("../img/reloj.png")}/>    
-                 <Image style ={{width:25,height:25, marginLeft:15}} source={require("../img/tacho-de-basura.png")}/>
-             </View>
-             <View style={styles.containerR}>
-                <RadioGroup style={styles.radioBtn}
-                    radioButtons={radioButtons} 
-                    onPress={onPressRadioButton} 
-                    />
-             </View>
-         </View>
-         <View style={styles.containerP}>
-             <View style={styles.containerOps}>
-                 <Text style={styles.txtOps}>¿Pregunta 1?</Text>
-                 <Text style={styles.txtOps2}>5 Seg </Text>
-                 <Image style={{width:25,height:25,marginRight:10}} source={require("../img/reloj.png")}/>    
-                 <Image style ={{width:25,height:25, marginLeft:15}} source={require("../img/tacho-de-basura.png")}/>
-             </View>
-             <View style={styles.containerR}>
-                <RadioGroup style={styles.radioBtn}
-                    radioButtons={radioButtons} 
-                    onPress={onPressRadioButton} 
-                    />
-             </View>
-         </View>
-         <View style={styles.containerP}>
-             <View style={styles.containerOps}>
-                 <Text style={styles.txtOps}>¿Pregunta 1?</Text>
-                 <Text style={styles.txtOps2}>5 Seg </Text>
-                 <Image style={{width:25,height:25,marginRight:10}} source={require("../img/reloj.png")}/>    
-                 <Image style ={{width:25,height:25, marginLeft:15}} source={require("../img/tacho-de-basura.png")}/>
-             </View>
-             <View style={styles.containerR}>
-                <RadioGroup style={styles.radioBtn}
-                    radioButtons={radioButtons} 
-                    onPress={onPressRadioButton} 
-                    />
-             </View>
-         </View>
-         <Pressable style={styles.btnMas}>
-            <Image style={{width:25,height:25}} source={require("../img/mas.png")}/>
-         </Pressable>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  )
-}
+    //Todos los datos del formulario
+    const [ titulo, setTitulo ] = useState("") 
+    const [ autor, setAutor ] = useState("")
+    const [ fechaCreacion, setFechaCreacion ] = useState("")
+    const [ fechaAct, setFechaAct ] = useState("")
+    const [ preguntas, setPreguntas ] = useState([{
+        titulo: "¿2+2?",
+        tiempo: 5,
+        respuestas: [
+            "5",
+            "4"
+        ]
+    }])
+
+    const [date, setDate] = useState("")
+    const [ opPregunta, setOpPregunta ] = useState({})
+    // const [ plantilla, setPlantilla] = useState({
+    //     titulo: "",
+    //     autor: auth.currentUser.email,
+    //     fechaCreacion: "",
+    //     fechaActualizacion: null,
+    //     preguntas: [{
+    //         titulo: "",
+    //         tiempo: 5, //En segundos
+    //         respuestas: [
+    //         ],
+    //         repCorrecta: null //Posicion en el array de respuestas
+    //     }]
+    // });
+
+    return (
+        <SafeAreaView style={styles.container}>
+            {/* container principal */}
+            <View style={styles.containerF}>
+
+                {/* Titulos */}
+                <TextInput style={styles.txtForm}
+                    placeholder = "Titulo de la plantilla"
+                    onChangeText = {(txt) => setTitulo(txt)}
+                    placeholderTextColor="#a0a0a0"
+                />
+
+                <Text style={styles.txtFecha}>Fecha de creación: {date.dateDay} {date.dateHour}</Text>
+
+                <ScrollView
+                    alwaysBounceVertical = {false}
+                >
+
+                    { preguntas.map((pregunta, index) => (                      
+                        <View style={styles.containerP} key={index}>
+
+                            {/* Pregunta contenedor "pregunta" */}
+                            <View style={styles.containerOps}>
+                                <TextInput 
+                                    style={styles.txtOps}
+                                    placeholderTextColor="#a0a0a0"
+                                    placeholder="Ingresa una pregunta"
+                                    onChangeText={ (txt) => {pregunta.titulo = txt}}
+                                />
+
+
+                            </View>
+                            <DropDownPicker
+                                    items={[
+                                        {label: 'Apple', value: 'apple'},
+                                        {label: 'Banana', value: 'banana'}
+                                    ]}       
+                                />
+                            {/* Pregunta contenedor respuesta */}
+                            <View style={styles.containerR}>
+                                <RadioButton.Group
+                                    onValueChange={value => setOpPregunta(value)}
+                                    value={opPregunta}
+                                >
+                                    <View>
+                                    <Text>First</Text>
+                                    <RadioButton value="first" />
+                                    </View>
+                                    <View>
+                                    <Text>Second</Text>
+                                    <RadioButton value="second" />
+                                    </View>
+                                    <View>
+                                    <Text>Second</Text>
+                                    <RadioButton value="third" />
+                                    </View>
+                                </RadioButton.Group>
+                            </View>
+                        </View>
+                    ))}
+                    
+                    <Pressable style={styles.btnMas}>
+                        <Image
+                        style={{ width: 25, height: 25 }}
+                        source={require("../img/mas.png")}
+                        />
+                    </Pressable>
+                    
+                </ScrollView>
+
+            </View>
+        </SafeAreaView>
+    );
+};
 
 const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        flex: 1
+    },
     containerF:{
-        flex:1,
-        marginLeft:15,
-        marginTop: 30
+        marginHorizontal: 20,
+        marginTop: 10,
     },
     txtForm:{
         fontSize: 30,
         color: "#000",
-        
     },
     txtFecha:{
-        fontSize:20,
-        fontWeight: "300"
+        color: "#a0a0a0",
+        marginTop: 10, 
+        marginBottom: 20
     },
     containerP:{
-        marginTop:15 ,
-        marginRight:15,
-        borderRadius: 5,
-        paddingHorizontal:20,
-        paddingVertical: 5,
-        borderColor: "#737373",
+        borderColor: '#D9D9D9',
         borderWidth: 1,
-        padding:20
+        borderRadius: 10,
+        padding: 20,
+        marginBottom: 10
     },
     containerOps:{
         flexDirection: "row",
@@ -135,4 +186,4 @@ const styles = StyleSheet.create({
     
 })
 
-export default CrearPlantilla
+export default CrearPlantilla;
