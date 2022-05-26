e462d537e77b2b6e6be0e73d777a2466a9dbc283
javierorozco2 <eorozco8@ucol.mx>
2022-05-25 22:23:05 -0500

    Ver plantillas v0.2

--- src/views/VerPlantillas.js
+++ src/views/VerPlantillas.js
@@ -22,6 +22,15 @@ const VerPlantillas = () => {
         })
     }, [])
 
+    const calcularTiempo = (obj) =>{
+        let tiempo = 0
+        obj.preguntas.forEach((val)=>{
+            tiempo += parseInt(val.tiempo)
+        })
+
+        return tiempo+"s"
+    }
+
     return (
         <SafeAreaView style={styles.container}>
 
@@ -29,26 +38,44 @@ const VerPlantillas = () => {
             <Text style={styles.titulo}>Ver plantillas</Text>
 
             {/* Plantillas */}
-            <ScrollView style={styles.plantillasScroll}>
+            <ScrollView style={styles.plantillasScroll} alwaysBounceVertical={false}>
 
                 {/* -------Una sola plantilla---------*/}
                 { plantillas.map((plantilla, index) =>(
 
-                    <Pressable >
-                        <View style={styles.plantCard} >
-                            <FontAwesome 
-                                style={styles.iconfaws}
-                                color={"#808080"}
-                                borderRadius="100" 
-                                size={20} 
-                                name="qrcode" 
-                            />
+                    <View style={styles.plantCard} >
+
+                        <View style={styles.viewTxt}>                            
                             <Text style={styles.plantCardTxt}>
                                 {plantilla.titulo}
                             </Text>
+                            <Text style={styles.tmpEstTxt}>
+                                Tiempo total: {calcularTiempo(plantilla)}
+                            </Text>
                         </View>
-                    </Pressable>
 
+                        <View style={styles.accbtn}>
+                            <Pressable
+                            >
+                                <FontAwesome 
+                                    style={styles.iconfaws}
+                                    color={"#808080"}
+                                    borderRadius="100" 
+                                    size={20} 
+                                    name="edit" 
+                                />
+                            </Pressable>
+                            <Pressable>
+                                <FontAwesome 
+                                    style={styles.iconfaws}
+                                    color={"#808080"}
+                                    borderRadius="100" 
+                                    size={20} 
+                                    name="trash" 
+                                />
+                            </Pressable>
+                        </View>
+                    </View>
                 )) }
                 {/* ------------------------------------ */}
             </ScrollView>
@@ -75,21 +102,41 @@ const styles = StyleSheet.create({
         borderColor: '#D9D9D9',
         borderWidth: 1,
         borderRadius: 10,
-        height: 50,
-        paddingHorizontal: 10,
+        height: 65,
+        paddingHorizontal: 15,
         alignItems: 'center',
         flexDirection: 'row',
         marginBottom: 10,
+        justifyContent: 'space-between'
     },
 
     iconfaws:{
-        marginLeft: 5
+        marginLeft: 5,
+        borderColor: '#d0d0d0',
+        borderWidth: 1,
+        borderRadius: 4,
+        padding: 4
     },
 
     plantCardTxt:{
-        marginLeft: 5,
+        fontSize: 16,
         color: '#808080',
         fontWeight: '500'
+    },
+
+    accbtn:{
+        flexDirection: 'row',
+        alignItems: 'center'
+    },
+
+    viewTxt:{
+        
+    },
+
+    tmpEstTxt:{
+        marginTop: 5,
+        color: '#0F74F2'
+
     }
 })
 
