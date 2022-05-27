d22b92ddca0803df4ee91dcb2c3e8ad080866795
Damianleonh <rleon2@ucol.mx>
2022-05-27 16:11:14 -0500

    alerta subir

--- src/views/Register.js
+++ src/views/Register.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Text, SafeAreaView, View, Image, TextInput, StyleSheet, Pressable, Modal, Alert } from 'react-native'
+import { Text, SafeAreaView, View, Image, TextInput, StyleSheet, Pressable, Modal, Alert, ScrollView } from 'react-native'
 import { Picker } from '@react-native-picker/picker';
 
 //Firebase
@@ -42,155 +42,152 @@ const Registro = ({ modalRegistro, setModalRegistro }) => {
     const auth = getAuth(app)
 
     createUserWithEmailAndPassword(auth, correo, password)
-        .then((userCredential)=>{
-            Alert.alert(
-              'Mensaje',
-              'Cuenta creada correctamente'
-            )
-            const user = userCredential.user
-            console.log(user)
+      .then((userCredential) => {
+        Alert.alert(
+          'Mensaje',
+          'Cuenta creada correctamente'
+        )
+        const user = userCredential.user
+        console.log(user)
 
-            //ENVIAR A FIRESTORE
-            const myDoc = doc(db, "Usuarios", correo)
-            const docData = {
-              correo: correo,
-              nombreUsuario: nombre + " " + apellido,
-              tipo:seleccionTipoUsuario + ""
-            }
+        //ENVIAR A FIRESTORE
+        const myDoc = doc(db, "Usuarios", correo)
+        const docData = {
+          correo: correo,
+          nombreUsuario: nombre + " " + apellido,
+          tipo: seleccionTipoUsuario + ""
+        }
 
-            setDoc(myDoc, docData).then(()=>{
-              console.log("Usuario registrado en firestore");
-              setModalRegistro(!modalRegistro);
-            }).catch(error=>{
-              console.log("Error al registrar en firestore: ",error)
-            })
-
+        setDoc(myDoc, docData).then(() => {
+          console.log("Usuario registrado en firestore");
+          setModalRegistro(!modalRegistro);
+        }).catch(error => {
+          console.log("Error al registrar en firestore: ", error)
         })
-        .catch(err => {
-            if (err.code === 'auth/invalid-email') {
-              Alert.alert(
-                'Error',
-                'El correo es invalido'
-              )
-            }else if(err.code === 'auth/weak-password' ){
-              Alert.alert(
-                'Error',
-                'La contraseña es debil'
-              )
-            }else if(err.code === 'auth/email-already-in-use' ){
-              Alert.alert(
-                'Error',
-                'La cuenta ya existe'
-              )
-            }else{
-              Alert.alert(
-                'Error',
-                'Error al registrar', err
-              )
-            }
-            console.log(err)
-            return
-        })
+
+      })
+      .catch(err => {
+        if (err.code === 'auth/invalid-email') {
+          Alert.alert(
+            'Error',
+            'El correo es invalido'
+          )
+        } else if (err.code === 'auth/weak-password') {
+          Alert.alert(
+            'Error',
+            'La contraseña es debil'
+          )
+        } else if (err.code === 'auth/email-already-in-use') {
+          Alert.alert(
+            'Error',
+            'La cuenta ya existe'
+          )
+        } else {
+          Alert.alert(
+            'Error',
+            'Error al registrar', err
+          )
+        }
+        console.log(err)
+        return
+      })
 
   }
 
   return (
     <SafeAreaView>
-      <Pressable
-        style={styles.btnRegresar}
-        onPress={() => setModalRegistro(!modalRegistro)}>
-        <Text
-          style={styles.textoBtnRegresar}
-        >
-          Cancelar</Text>
-      </Pressable>
-
-      <Image
-        style={styles.imagen}
-        source={require('../img/logo.jpg')}
-      />
-
-      {/* Inputs y botones */}
-      <View style={styles.contenedorBtnInputs}>
-        <TextInput
-          onChangeText={(txt) => setCorreo(txt)}
-          style={styles.input}
-          placeholder='Correo'
-          keyboardType='email-address'
-          placeholderTextColor="#a0a0a0"
-
-        />
-
-        <TextInput
-          onChangeText={(txt) => setPassword(txt)}
-          style={styles.input}
-          placeholder='Contraseña'
-          secureTextEntry
-          placeholderTextColor="#a0a0a0"
-
-        />
-        <TextInput
-          onChangeText={(txt) => setResPassword(txt)}
-          style={styles.input}
-          placeholder='Repetir Contraseña'
-          secureTextEntry
-          placeholderTextColor="#a0a0a0"
-
-        />
-        <TextInput
-          onChangeText={(txt) => setNombre(txt)}
-          style={styles.input}
-          placeholder='Nombre/s'
-          placeholderTextColor="#a0a0a0"
-
-        />
-
-        <TextInput
-          onChangeText={(txt) => setApellido(txt)}
-          style={styles.input}
-          placeholder='Apellidos'
-          placeholderTextColor="#a0a0a0"
-
-        />
-
-
-        <View style={styles.containerPicker}>
-          <Text style={styles.tipouserTxt}>Selecciona el tipo de usuario:</Text>
-          <Picker
-            selectedValue={seleccionTipoUsuario}
-            onValueChange={(itemValue,itemIndex) => 
-              setSeleccionTipoUsuario(itemValue)
-            }
+      <ScrollView>
+        <Pressable
+          style={styles.btnRegresar}
+          onPress={() => setModalRegistro(!modalRegistro)}>
+          <Text
+            style={styles.textoBtnRegresar}
           >
-            <Picker.Item label="Profesor" value="usuarioProfesor" />
-            <Picker.Item label="Alumno" value="usuarioAlumno" />
-          </Picker>
+            Cancelar</Text>
+        </Pressable>
 
+        <Image
+          style={styles.imagen}
+          source={require('../img/logo.jpg')}
+        />
+
+        {/* Inputs y botones */}
+        <View style={styles.contenedorBtnInputs}>
+          <TextInput
+            onChangeText={(txt) => setCorreo(txt)}
+            style={styles.input}
+            placeholder='Correo'
+            keyboardType='email-address'
+            placeholderTextColor="#a0a0a0"
+
+          />
+
+          <TextInput
+            onChangeText={(txt) => setPassword(txt)}
+            style={styles.input}
+            placeholder='Contraseña'
+            secureTextEntry
+            placeholderTextColor="#a0a0a0"
+
+          />
+          <TextInput
+            onChangeText={(txt) => setResPassword(txt)}
+            style={styles.input}
+            placeholder='Repetir Contraseña'
+            secureTextEntry
+            placeholderTextColor="#a0a0a0"
+
+          />
+          <TextInput
+            onChangeText={(txt) => setNombre(txt)}
+            style={styles.input}
+            placeholder='Nombre/s'
+            placeholderTextColor="#a0a0a0"
+
+          />
+
+          <TextInput
+            onChangeText={(txt) => setApellido(txt)}
+            style={styles.input}
+            placeholder='Apellidos'
+            placeholderTextColor="#a0a0a0"
+
+          />
+
+
+          <View style={styles.containerPicker}>
+            <Text style={styles.tipouserTxt}>Selecciona el tipo de usuario:</Text>
+            <Picker
+              selectedValue={seleccionTipoUsuario}
+              onValueChange={(itemValue, itemIndex) =>
+                setSeleccionTipoUsuario(itemValue)
+              }
+            >
+              <Picker.Item label="Profesor" value="usuarioProfesor" />
+              <Picker.Item label="Alumno" value="usuarioAlumno" />
+            </Picker>
+
+          </View>
+
+
+          {/* Contenedor de botones */}
+          <View style={styles.contenedorBotones}>
+
+            {/* Boton registrar */}
+            <Pressable
+              style={styles.button}
+              onPress={() => crearUsuario()}
+            >
+              <Text style={styles.buttonText}>
+                Registrate
+              </Text>
+            </Pressable>
+          </View>
         </View>
 
 
-        {/* Contenedor de botones */}
-        <View style={styles.contenedorBotones}>
 
-          {/* Boton registrar */}
-          <Pressable
-            style={styles.button}
-            onPress={() => crearUsuario()}
-          >
-            <Text style={styles.buttonText}>
-              Registrate
-            </Text>
-          </Pressable>
-        </View>
-      </View>
-
-
-
-      {/* Ultima imagen */}
-      <Image
-        source={require('../img/BOTTOMAPP.png')}
-      />
-
+      </ScrollView>
 
 
     </SafeAreaView>
@@ -221,7 +218,7 @@ const styles = StyleSheet.create({
     padding: 15,
     borderRadius: 10,
     alignItems: 'center',
-    marginTop: 40
+    
   },
 
   buttonText: {
@@ -254,7 +251,7 @@ const styles = StyleSheet.create({
 
   containerPicker: {
     padding: 15,
-    marginBottom: 40
+    
   },
 
   tipouserTxt: {
